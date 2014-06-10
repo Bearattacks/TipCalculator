fb80bc910523e599ca2814e4945bf5bb08105f78
Tim Collins <timcollinsdesign@gmail.com>
2014-06-10 11:23:09 -0700

    First Commit

---
+++ TipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  TipCalculator
+//
+//  Created by Tim Collins on 6/9/14.
+//  Copyright (c) 2014 Tim Collins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ TipCalculator/TipViewController.m
@@ -0,0 +1,38 @@
+//
+//  TipViewController.m
+//  TipCalculator
+//
+//  Created by Tim Collins on 6/9/14.
+//  Copyright (c) 2014 Tim Collins. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
